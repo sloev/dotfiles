b468c9c720d487d875c9c3e4a088b245cfc5e896
sloev <johannes.valbjorn@gmail.com>
2015-09-17 11:21:01 +0200

    changed install.sh mistakes and made more verbose

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #/bin/bash
-sudo apt-get install git molly-guard vim python-setup-tools tmux zsh
+sudo apt-get install molly-guard vim python-setuptools tmux zsh
+echo "installing pip"
 sudo easy_install pip
-sudo pip install ipython
+echo "pip installing ipython termcolor"
+sudo pip install ipython termcolor
 
+echo "changing shell to zsh"
 chsh -s /bin/zsh $(whoami)
+echo "cloning vundlevim"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "installing vim plugins"
 vim +PluginInstall +qall
 
 echo "trying to copy all dotfiles to root"
 
 cp -r .* ~/ 2>/dev/null
+
+echo "install done!!!"
